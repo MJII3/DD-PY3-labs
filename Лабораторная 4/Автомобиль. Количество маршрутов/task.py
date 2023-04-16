19aeea08815bd88222d503375ba59d41332ba839
MJII3 <115094714+MJII3@users.noreply.github.com>
2023-04-16 19:36:06 +0300

    Lab 4 task 1

--- Лабораторная 4/Автомобиль. Количество маршрутов/task.py
+++ Лабораторная 4/Автомобиль. Количество маршрутов/task.py
@@ -10,8 +10,31 @@ def car_paths(n: int, m: int) -> List[List[int]]:
 
     :return: Новую таблицу с посчитанным количеством маршрутов в каждую клетку
     """
-    ...  # TODO решить задачу с помощью динамического программирования
+        check = (n,m)
+    if not isinstance(n, int) or not isinstance(m, int):
+        raise TypeError
 
+    if n < 0 or m < 0:
+        raise  ValueError
+
+    if n == 0 or m == 0:
+        return 1
+
+
+    table = []     #Cоздаем таблицу
+    for _ in range(n):
+        table.append([0] * m)
+
+    for i in range(n):
+        table[i][0] = 1
+
+    for j in range(m):
+        table[0][j] = 1
+
+    for i in range(1, n):
+        for j in range(1, m):
+            table[i][j] = table[i - 1][j - 1] + table[i][j - 1] + table[i - 1][j]
+    return table
 
 if __name__ == '__main__':
     paths = car_paths(4, 2)
