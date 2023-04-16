7024210590167920dc54fd03ad2986a70b58b168
MJII3 <115094714+MJII3@users.noreply.github.com>
2023-04-16 19:59:27 +0300

    Lab 5

--- Лабораторная 5/Платная лестница. Дейкстра/task.py
+++ Лабораторная 5/Платная лестница. Дейкстра/task.py
@@ -11,10 +11,19 @@ def stairway_path(graph: nx.DiGraph) -> Union[float, int]:
     :param graph: Взвешенный направленный граф NetworkX, по которому надо рассчитать стоимости кратчайших путей
     :return: минимальная стоимость подъема на верхнюю ступень
     """
-    ...  # TODO c помощью функции из модуля networkx найти стоимость кратчайшего пути до последней лестницы
-
+    a =graph.number_of_nodes(-1)
+    min_cost = nx.dijkstra_path_length(graph, 0, a)
+    return min_cost
 
 if __name__ == '__main__':
     stairway = (5, 11, 43, 2, 23, 43, 22, 12, 6, 8)
-    stairway_graph = ...  # TODO записать взвешенный граф, а лучше написать функцию, которая формирует граф по стоимости ступеней
+    stairway_graph = nx.DiGraph()
+    stairway_graph.add_weighted_edges_from([
+        (0, 1, 5),
+        (0, 2, 11),
+        (1, 2, 11),
+        (1, 3, 43),
+        (2, 3, 43),
+        (2, 4, 2),
+        (3, 4, 2)])
     print(stairway_path(stairway_graph))  # 72
